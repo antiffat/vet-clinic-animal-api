9a27627d8d585727ee6b1929af08dfc5d7bf5311
s28222 <s28222@pjwstk.edu.pl>
2024-04-11 21:19:46 +0200

    Implementation of VisitController with CRUD operations

---
+++ VetClinicAnimalApi.Service/Controllers/VisitController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using VetClinicAnimalApi.Service.Interfaces;
+using VetClinicAnimalApi.Service.Models;
+using VetClinicAnimalApi.Service.Repositories;
+
+namespace VetClinicAnimalApi.Service.Controllers;
+
+public class VisitController : ControllerBase
+{
+    private readonly IVisitRepository _visitRepository;
+
+    public VisitController()
+    {
+        _visitRepository = new VisitRepository();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<List<Visit>> GetByAnimalId(int animalId)
+    {
+        return _visitRepository.GetVisitsByAnimalId(animalId);
+    }
+
+    [HttpPost]
+    public IActionResult AddVisit(Visit visit)
+    {
+        _visitRepository.AddVisit(visit);
+        return CreatedAtAction(nameof(GetByAnimalId), new { animalId = visit.AnimalId }, visit);
+    }
+}
